9079ef3e09d344d5e017951ddc7899379c1ab4e9
Rakshitha-33 <4mu22cs069@mysururoyal.org>
2026-02-03 17:32:49 +0530

    I implement the Inhertiance

---
+++ day2.py
@@ -0,0 +1,22 @@
+class Book :
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+    def display_book_details(self):
+        print("The title of the book is",self.title,"and the author is",self.author)
+    
+
+class IssuedBook(Book):
+    def __init__(self, title, author,issued_to,issued_date):
+        super().__init__(title, author)
+        self.issued_to=issued_to
+        self.issued_date=issued_date
+    def display_book_details(self):
+        super().display_book_details()
+        print("The book is issued in",self.issued_date,"to",self.issued_to)
+       
+        
+
+
+obj=IssuedBook("Harry Potter and the Philosopher’s Stone","J.K. Rowling","03-02-2026","09:00 AM")
+obj.display_book_details()
