a11e0eccb73a384de2f1f4885b493e0a57f2becc
Rakshitha-33 <4mu22cs069@mysururoyal.org>
2026-02-10 18:35:16 +0530

    Dunder / Special Methods Example

---
+++ day9/Mobile.py
@@ -0,0 +1,21 @@
+class Mobile:
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+    def __eq__(self, other):
+        if isinstance(other, Mobile):
+            return self.brand == other.brand and self.model == other.model
+        return False
+
+
+
+m1 = Mobile("Apple", "iPhone 15", 80000)
+m2 = Mobile("Apple", "iPhone 15", 75000)
+m3 = Mobile("Samsung", "S23", 70000)
+
+
+print(m1 == m2)  
+print(m1 == m3) 
+print(m2 == m3)  
